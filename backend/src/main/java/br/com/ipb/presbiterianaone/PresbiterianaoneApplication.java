6047ca6664a992b12dd9807fcc70b7169bb9c70a
RevRicardo <revricardolacerda@gmail.com>
2025-10-24 17:09:51 -0300

    Initialize project

---
+++ backend/src/main/java/br/com/ipb/presbiterianaone/PresbiterianaoneApplication.java
@@ -0,0 +1,13 @@
+package br.com.ipb.presbiterianaone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresbiterianaoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresbiterianaoneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/ipb/presbiterianaone/PresbiterianaoneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ipb.presbiterianaone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresbiterianaoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
